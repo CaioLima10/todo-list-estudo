597d6fc33a26b87a3a73595fbb1f4d3fbb1ea24d
CaioLima <caiohenrys50@gmail.com>
2026-05-31 16:16:25 -0300

    component button icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Text } from "./components/text/text";
 import LogoTodoList from "./assets/image/logo-todo-list.svg?react"
 import IconSpinner from "./assets/image/spinner.svg?react"
+import IconTrash from "./assets/image/trash.svg?react"
 import IconPlus from "./assets/image/plus.svg?react"
 import { Icon } from "./components/icon/icon";
 import { Badge } from "./components/badge/badge";
 import { Button } from "./components/button/button";
+import { ButtonIcon } from "./components/buttonIcon/button-icon";
 
 export function App() {
   return (
@@ -16,8 +18,13 @@ export function App() {
       <Icon svg={IconSpinner}/>
       <Badge variant="primary" size="sm">5</Badge>
       <Badge variant="secondary" size="sm">2 de 5</Badge>
-      <Button disabled icon={IconPlus}>Criar</Button>
-      <Button>Criar</Button>
+      <Button disabled icon={IconPlus}>Nova tarefa</Button>
+      <Button icon={IconPlus}>Nova tarefa</Button>
+
+      <ButtonIcon icon={IconTrash}/> 
+      <ButtonIcon disabled icon={IconTrash}/> 
+      <ButtonIcon  icon={IconTrash} variant="secondary"/> 
+      <ButtonIcon  icon={IconTrash} variant="tertiary"/> 
     </>
   )
 }
